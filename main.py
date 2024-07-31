d8da3bd4c2b05ca6d9806b7a628554ca656cbcd7
Zac Smith <zrs3141592@gmail.com>
2024-07-30 23:10:04 -0600

    👍 IMPROVE: main done -working

--- main.py
+++ main.py
@@ -13,24 +13,34 @@ openai_key = os.getenv('OPENAI_API_KEY2')
 pinecone_key = os.getenv('PINECONE_API_KEY')
 
 # the prompt: we will be changing this soon
-prompt = "hello world!"
 
 # Note: we must use the same embedding model that we used when uploading the docs
 embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL)
-document_vectorstore = PineconeVectorStore.from_documents(index_name=PINECONE_INDEX, embedding=embeddings)
+
+# Querying the vector database for "relevant" docs then create a retriever
+document_vectorstore = PineconeVectorStore(index_name=PINECONE_INDEX, embedding=embeddings)
 retriever =document_vectorstore.as_retriever()
 
-llm = ChatOpenAI(temperature=0.7)
+llm = ChatOpenAI(temperature=0.7, model_name="gpt-4o-mini")
 template = PromptTemplate(template="{query} Context: {context}", input_variables=["query", "context"])
-# Querying the vector database for "relevant" docs then create a retriever
+
+prompt = "Can you tell me about the best overall methods for day trading?"
 
 # create a context by using the retriever and getting the relevant docs based on the prompt
+context = retriever.get_relevant_documents(prompt)
+
 
 # show the thought process by looping over all relevant docs, showing the source and the content
-
-
+for doc in context:
+  print(f"Source: {doc.metadata}\nContext: {doc.page_content}\n\n")
+  print("******************************")
+  
 # build a prompt template using the query and the context and build the prompt with context
+prompt_with_context = template.invoke({"query": prompt, "context": context})
 
 
 # Asking the LLM for a response from our prompt with the provided context using ChatOpenAI and invoking it
+results = llm.invoke(prompt_with_context)
+
 # Then print the results content
+print(results.content)
